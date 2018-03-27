82a412bc8d87d6942c1eda4bff3df932deda279f
Risdyanto Kurniawan <risdyanto@rajamobil.net>
2018-03-27 14:19:44 +0700

    FOR TESTING ONLY

---
+++ README.md
@@ -0,0 +1,9 @@
+# Testing login page 
+
+This is my testing for login example
+
+1) clone my repo
+
+2) npm install
+
+3) npm start
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,46 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { User } from '../_models/index';
+import { UserService } from '../_services/index';
+
+@Component({
+    moduleId: module.id.toString(),
+    templateUrl: 'home.component.html'
+})
+
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    users: User[] = [];
+
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+
+    chartOptions = {
+        responsive: true
+    };
+    
+    chartData = [
+        { data: [330, 600, 260, 700], label: 'Account A' },
+        { data: [120, 455, 100, 340], label: 'Account B' },
+        { data: [45, 67, 800, 500], label: 'Account C' }
+    ];
+    
+    chartLabels = ['January', 'February', 'Mars', 'April'];
+    
+    //   onChartClick(e) {
+    //     console.log(e);
+    //   }
+}
